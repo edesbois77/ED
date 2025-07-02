d9f0338883719c5ac1e6512f5a28a605b299e0d7
edesbois77 <edesbois@yahoo.co.uk>
2025-07-02 21:42:47 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  base: '/ED/',
-  plugins: [react()]
+  base: '/ED/', // this is correct for GitHub Pages
+  plugins: [react()],
+  root: '.', // root directory of your project
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html')
+    }
+  }
 })
