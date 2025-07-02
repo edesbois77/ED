ae46f1c5b15fc1e4888d3d20db7b6f5803d3bbb7
edesbois77 <edesbois@yahoo.co.uk>
2025-07-02 22:08:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ED/', // ✅ This is the key line to add
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
