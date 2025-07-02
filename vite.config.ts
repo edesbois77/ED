bae1f86287e0991c75001db0a5f61a41a838e2b0
edesbois77 <edesbois@yahoo.co.uk>
2025-07-02 21:12:18 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
-
 import { defineConfig } from 'vite'
 
 export default defineConfig({
