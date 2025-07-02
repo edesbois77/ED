053d93deeca413151a6df7584d2fd12933f95c1c
edesbois77 <edesbois@yahoo.co.uk>
2025-07-02 22:13:28 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ED/', // ✅ This is the key line to add
+  base: '/ED/', // this is crucial for GitHub Pages to resolve paths
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
